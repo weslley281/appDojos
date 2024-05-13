36c9f3bc61fa8950a1826699c6018fda7f2a0fc3
Weslley H V Ferraz <weslleyhenrique800@gmail.com>
2024-05-13 05:38:50 -0400

    a

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    icon: path.join(__dirname, './images/logo-kenshydokan.ico'),
+  });
+
+  mainWindow.setMenu(null);
+
+  mainWindow.loadURL('http://localhost/Academia%20e%20Dojo/index.php'); // Substitua "https://seusite.com" pela URL do seu site/sistema web
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
